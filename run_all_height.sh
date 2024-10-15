7f2d68f0efe1aebfd08cf74447044dda6dd1a1ef
Abhinav Kumar | अभिनव कुमार <abhinav3663@gmail.com>
2024-10-15 14:15:59 -0400

    Update run_all_height.sh

--- run_all_height.sh
+++ run_all_height.sh
@@ -9,3 +9,9 @@ bash run_new.sh 21 height 0 0 0.5334
 bash run_new.sh 24 height 0 0 0.6096
 bash run_new.sh 27 height 0 0 0.6858
 bash run_new.sh 30 height 0 0 0.7620
+
+bash run_new.sh -6 height 0 0 -0.1524
+bash run_new.sh -12 height 0 0 -0.3048
+bash run_new.sh -18 height 0 0 -0.4572
+bash run_new.sh -24 height 0 0 -0.6096
+bash run_new.sh -27 height 0 0 -0.6858
